4aed8afddea610bb18ed88ce56fc3e540f5d3347
Amaury Pochon <amaury.pochon@univ-tlse3.fr>
2017-03-23 15:12:14 +0100

    Entity Article

---
+++ Projet_JEE/Projet_JEE-ejb/src/java/entities/Article.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Amaury_PC
+ */
+@Entity
+@Table(name = "article")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Article.findAll", query = "SELECT a FROM Article a")
+    , @NamedQuery(name = "Article.findById", query = "SELECT a FROM Article a WHERE a.id = :id")
+    , @NamedQuery(name = "Article.findByPrixHt", query = "SELECT a FROM Article a WHERE a.prixHt = :prixHt")
+    , @NamedQuery(name = "Article.findByTauxTva", query = "SELECT a FROM Article a WHERE a.tauxTva = :tauxTva")
+    , @NamedQuery(name = "Article.findByStock", query = "SELECT a FROM Article a WHERE a.stock = :stock")})
+public class Article implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "LIB")
+    private String lib;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRIX_HT")
+    private double prixHt;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "TAUX_TVA")
+    private float tauxTva;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "STOCK")
+    private int stock;
+
+    public Article() {
+    }
+
+    public Article(Integer id) {
+        this.id = id;
+    }
+
+    public Article(Integer id, String lib, double prixHt, float tauxTva, int stock) {
+        this.id = id;
+        this.lib = lib;
+        this.prixHt = prixHt;
+        this.tauxTva = tauxTva;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLib() {
+        return lib;
+    }
+
+    public void setLib(String lib) {
+        this.lib = lib;
+    }
+
+    public double getPrixHt() {
+        return prixHt;
+    }
+
+    public void setPrixHt(double prixHt) {
+        this.prixHt = prixHt;
+    }
+
+    public float getTauxTva() {
+        return tauxTva;
+    }
+
+    public void setTauxTva(float tauxTva) {
+        this.tauxTva = tauxTva;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Article)) {
+            return false;
+        }
+        Article other = (Article) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Article[ id=" + id + " ]";
+    }
+    
+}
